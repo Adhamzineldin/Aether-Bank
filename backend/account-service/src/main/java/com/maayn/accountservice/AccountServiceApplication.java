351fef804480b9d88bd1ca0f6d5e792a2c09abf6
Adham Zineldin <Mohalya3@gmail.com>
2026-04-06 04:33:44 +0200

    initialize veld

--- backend/account-service/src/main/java/com/maayn/accountservice/AccountServiceApplication.java
+++ backend/account-service/src/main/java/com/maayn/accountservice/AccountServiceApplication.java
@@ -1,12 +1,17 @@
 package com.maayn.accountservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
 
 @SpringBootApplication
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
+      
         SpringApplication.run(AccountServiceApplication.class, args);
     }
 
