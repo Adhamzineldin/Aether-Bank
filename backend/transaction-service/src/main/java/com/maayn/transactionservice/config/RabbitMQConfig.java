27257d01763eaede7840bf01e9ec2eeb968efb61
Adham Zineldin <Mohalya3@gmail.com>
2026-04-07 18:39:19 +0200

    Update RabbitMQ configuration to use JacksonJsonMessageConverter for message conversion

--- backend/transaction-service/src/main/java/com/maayn/transactionservice/config/RabbitMQConfig.java
+++ backend/transaction-service/src/main/java/com/maayn/transactionservice/config/RabbitMQConfig.java
@@ -1,10 +1,10 @@
 package com.maayn.transactionservice.config;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class RabbitMQConfig {
@@ -12,7 +12,6 @@ public class RabbitMQConfig {
     public static final String EXCHANGE = "transaction_exchange";
     public static final String QUEUE = "transaction_notification_queue";
     public static final String ROUTING_KEY = "transaction_routing_key";
-
 
     @Bean
     public Queue queue() {
@@ -23,16 +22,14 @@ public class RabbitMQConfig {
     public TopicExchange exchange() {
         return new TopicExchange(EXCHANGE);
     }
-    
+
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
     @Bean
-    public MessageConverter converter() {   
-        return new MessageConverter();
+    public MessageConverter converter() {
+        return new JacksonJsonMessageConverter();
     }
-
-
 }
--- backend/transaction-service/src/main/java/com/maayn/transactionservice/service/EventPublisherService.java
+++
@@ -1,4 +0,0 @@
-package com.maayn.transactionservice.service;
-
-public class EventPublisherService {
-}
