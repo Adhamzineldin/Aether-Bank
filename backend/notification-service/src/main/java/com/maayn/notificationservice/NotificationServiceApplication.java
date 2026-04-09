a70f43c5d8a325e51336ef866e2b2c49676e20e1
Ayman Elfeky <aywork73@gmail.com>
2026-04-10 00:48:27 +0200

    feat: Initialize Notification Service

--- backend/notification-service/src/main/java/com/maayn/notificationservice/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/maayn/notificationservice/NotificationServiceApplication.java
@@ -1,9 +1,14 @@
 package com.maayn.notificationservice;
 
+import maayn.veld.generated.VeldAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Check why
+@Import(VeldAutoConfiguration.class)
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
