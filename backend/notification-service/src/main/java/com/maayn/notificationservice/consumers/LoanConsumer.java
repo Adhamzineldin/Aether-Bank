f6b09bf808a6b1107991e3715e8cf231e9c8e29d
Ayman Elfeky <aywork73@gmail.com>
2026-04-10 11:58:04 +0200

    feat: Configure MongoDb with indexes - Create Config folder for mongoDB and other configs will be needed such as mail

---
+++ backend/notification-service/src/main/java/com/maayn/notificationservice/consumers/LoanConsumer.java
@@ -0,0 +1,4 @@
+package com.maayn.notificationservice.consumers;
+
+public class LoanConsumer {
+}
---
+++ backend/notification-service/src/main/java/com/maayn/notificationservice/services/notification/NotificationServiceImpl.java
@@ -0,0 +1,56 @@
+package com.maayn.notificationservice.services.notification;
+
+import maayn.veld.generated.errors.*;
+import maayn.veld.generated.models.*;
+import maayn.veld.generated.services.INotificationService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationServiceImpl implements INotificationService {
+    @Override
+    public List<NotificationItem> listNotifications() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public NotificationItem getNotification(String id) throws GetNotificationException, Exception {
+        return null;
+    }
+
+    @Override
+    public NotificationItem sendNotification(SendNotificationInput input) throws SendNotificationException, Exception {
+        return null;
+    }
+
+    @Override
+    public NotificationItem retryNotification(String id) throws RetryNotificationException, Exception {
+        return null;
+    }
+
+    @Override
+    public List<NotificationTemplate> listTemplates() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public NotificationTemplate getTemplate(String id) throws GetTemplateException, Exception {
+        return null;
+    }
+
+    @Override
+    public NotificationTemplate createTemplate(CreateTemplateInput input) throws CreateTemplateException, Exception {
+        return null;
+    }
+
+    @Override
+    public NotificationTemplate updateTemplate(String id, UpdateTemplateInput input) throws UpdateTemplateException, Exception {
+        return null;
+    }
+
+    @Override
+    public NotificationTemplate deactivateTemplate(String id) throws DeactivateTemplateException, Exception {
+        return null;
+    }
+}
