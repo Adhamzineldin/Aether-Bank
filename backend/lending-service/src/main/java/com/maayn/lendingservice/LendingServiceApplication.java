ee12f25d5cfdf418d9e3ef74af21e607d6f34923
red <162513969+redoxxa21@users.noreply.github.com>
2026-04-14 01:28:36 +0200

    feat: initiated the lending service

--- backend/lending-service/src/main/java/com/maayn/lendingservice/LendingServiceApplication.java
+++ backend/lending-service/src/main/java/com/maayn/lendingservice/LendingServiceApplication.java
@@ -1,9 +1,14 @@
 package com.maayn.lendingservice;
 
+import maayn.veld.generated.VeldAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Import(VeldAutoConfiguration.class)
 public class LendingServiceApplication {
 
     public static void main(String[] args) {
