399a3578584dc3082f8f5223006e03e81edf3ba1
Mohamed Ali <mooomali15@gmail.com>
2026-04-17 19:10:42 +0200

    Add-Card-Services

--- backend/card-service/src/main/java/com/maayn/cardservice/CardServiceApplication.java
+++ backend/card-service/src/main/java/com/maayn/cardservice/CardServiceApplication.java
@@ -1,9 +1,12 @@
 package com.maayn.cardservice;
 
+import maayn.veld.generated.VeldAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(VeldAutoConfiguration.class)
 public class CardServiceApplication {
 
     public static void main(String[] args) {
